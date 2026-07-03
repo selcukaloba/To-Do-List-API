29d893a6633e02990429f36d004419197a52a9e8
selcukaloba <alobaselcuk@gmail.com>
2026-07-03 12:43:51 +0300

    initial project setup

---
+++ src/main/java/com/selcukaloba/to_do_api_project/ToDoApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.selcukaloba.to_do_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selcukaloba/to_do_api_project/ToDoApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selcukaloba.to_do_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
