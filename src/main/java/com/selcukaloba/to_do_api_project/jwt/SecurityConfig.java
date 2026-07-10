ee13bdf7753e6ec3e1972bd1871a4a82d7260004
selcukaloba <alobaselcuk@gmail.com>
2026-07-10 12:11:16 +0300

    sec. config added

---
+++ src/main/java/com/selcukaloba/to_do_api_project/jwt/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.selcukaloba.to_do_api_project.jwt;
+
+import com.selcukaloba.to_do_api_project.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.csrf(csrf->csrf.disable());
+
+        http.authorizeHttpRequests(auth->auth
+                .requestMatchers("/auth/**", "/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
+                .anyRequest().authenticated());
+
+        http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authenticationProvider(authenticationProvider);
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+
+    }
+}
